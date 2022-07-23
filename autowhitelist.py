07b0a836cdf8553e00a3ae774460f682c1ca41be
zhishixiang <1015256551@qq.com>
2022-07-23 14:12:09 +0800

    写入白名单

--- autowhitelist.py
+++ autowhitelist.py
@@ -2,6 +2,7 @@ from time import sleep
 import socketio
 from loguru import logger
 import json
+import httpx
 
 sio = socketio.Client()
 
@@ -13,8 +14,8 @@ except:
     logger.error("读取配置文件失败，请检查配置文件是否存在且格式是否正确")
 
 try:
-    with open("whitelist.json", "r", encoding='UTF-8') as f:
-        whitelist = json.load(f)
+    whitelistFile = open("whitelist.json", "r", encoding='UTF-8')
+    whitelist = json.load(whitelistFile)
     logger.info("读取白名单列表中")
     for i in whitelist:
         logger.info("uuid:" + i['uuid'])
@@ -22,6 +23,7 @@ try:
     logger.info("读取完毕，请确认是否能正确读取")
 except:
     logger.error("读取whitelist.json失败，请检查文件是否存在或是否损坏")
+
 
 @sio.on('connect')
 def on_connect():
@@ -52,7 +54,19 @@ def isReg(data):
 def newMission(data):
     id = data["id"]
     logger.info("新玩家%s已通过入服考试，即将添加白名单" % id)
-    return "success"
+    respond = httpx.get("https://api.mojang.com/users/profiles/minecraft/" + id)
+    if respond.status_code == "204":
+        logger.error("白名单添加失败：该玩家不存在")
+        return {"status": "failed", "reason": "player not found"}
+    elif respond.status_code == "200":
+        playerdata = json.loads(respond.read())
+        whitelist.insert(playerdata)
+        whitelistFile.write(whitelist)
+
+        return {"status": "success"}
+    else:
+        logger.error("白名单添加失败：无法查询玩家信息，请确保网络畅通")
+        return {"status": "failed", "reason": "network error"}
 
 
 sio.connect('ws://127.0.0.1:8090/')
